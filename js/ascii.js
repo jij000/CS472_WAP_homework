b1a35c50b29531b735333ebddf8378807e47ae49
jiangyj <jij000@gmail.com>
2019-09-09 19:02:40 -0500

    lab07

--- js/ascii.js
+++ js/ascii.js
@@ -5,34 +5,50 @@ fontSize['Medium'] = '12pt';
 fontSize['Large'] = '16pt';
 fontSize['Extra Large'] = '24pt';
 fontSize['XXL'] = '32pt';
+let playControl;
+let currText = '';
+let animeList = [];
+let playIndex = 0;
 
-window.onload = function() {
-    document.getElementById('animation').onchange = function() {
-        document.getElementById('text-area').value = ANIMATIONS[this.value];
+let animationChange = function () {
+    document.getElementById('text-area').value = ANIMATIONS[document.getElementById('animation').value];
+};
+let turboChange = function () {
+    clearInterval(playControl);
+    if (document.getElementById('turbo').checked == '') {
+        playControl = setInterval(playAscii, 250);
+    } else {
+        playControl = setInterval(playAscii, 50);
     }
-    document.getElementById('fontsize').onchange = function() {
+};
+
+window.onload = function () {
+    document.getElementById('animation').onchange = animationChange;
+    document.getElementById('fontsize').onchange = function () {
         // Tiny (7pt), Small (10pt), Medium (12pt), Large (16pt), Extra Large (24pt), XXL (32pt)
         document.getElementById('text-area').style.fontSize = fontSize[this.value];
     }
-    document.getElementById('turbo').onchange = function() {
-        if (this.checked == '') {
-            setInterval(playAscii, 250);
-        } else {
-            setInterval(playAscii, 50);
-        }
-    }
-    document.getElementById('start').onclick = function() {
+    document.getElementById('turbo').onchange = turboChange;
+    document.getElementById('start').onclick = function () {
         this.disabled = 'disabled';
         document.getElementById('stop').disabled = '';
-        setInterval(playAscii, 250);
+        currText = document.getElementById('text-area').value;
+        animeList = currText.split("=====\n");
+        playIndex = 0;
+        turboChange();
     }
-    document.getElementById('stop').onclick = function() {
+    document.getElementById('stop').onclick = function () {
         this.disabled = 'disabled';
         document.getElementById('start').disabled = '';
-        setInterval(playAscii, 250);
+        clearInterval(playControl);
+        animationChange();
     }
 }
 
-var playAscii = function() {
-
+var playAscii = function () {
+    document.getElementById('text-area').value = animeList[playIndex];
+    playIndex += 1;
+    if (playIndex >= animeList.length) {
+        playIndex = 0;
+    }
 }
